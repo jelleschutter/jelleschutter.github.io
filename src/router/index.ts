828034322fd801d31855f5fb3c700156b957fa8d
Jelle Schutter <jelle@schutter.xyz>
2021-02-13 02:50:38 +0100

    add meta title option

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,9 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     name: 'home',
     component: Home,
+    meta: {
+      title: 'Home',
+    },
   },
   {
     path: '/:pathMatch(.*)*',
@@ -18,6 +21,16 @@ const routes: RouteRecordRaw[] = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const { title } = to.meta;
+  if (typeof title === 'undefined') {
+    document.title = 'Jelle Schutter';
+  } else {
+    document.title = `${title} / Jelle Schutter`;
+  }
+  next();
 });
 
 export default router;
